32d422ea09fc5196b226a9c90a87911b1ba7954a
Nathan Lee <nathan.l.lee123@gmail.com>
2019-01-02 21:48:09 -0800

    Wrote script, but selenium isn't very compliant so I'm giving up for now

--- corrector.py
+++ corrector.py
@@ -1,10 +1,58 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
-from robobrowser import RoboBrowser
-import re
-browser = RoboBrowser(history = True)
-browser.open('https://www.strava.com/athlete/training')
+import time
+driver = webdriver.Firefox()
 
-email = browser.get_form(id='email')
-email['q'].value = 'nathan.l.lee123@gmail.com'
-password = browser.get_form(id='password')
+email = 'nathan.l.lee123@gmail.com'#raw_input('Email: ')
+password = '3xFXwaK951od'#raw_input('Password: ')
+
+
+driver.get('https://www.strava.com/athlete/training')
+
+#signin
+driver.find_element_by_id('email').send_keys(email)
+driver.find_element_by_id('password').send_keys(password)
+driver.find_element_by_id('login-button').click()
+print('Signed in')
+time.sleep(1.5)
+
+#parse with bs
+
+
+urllist = []
+activitycounter = 0
+
+while True:
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    for activityurls in soup.find_all('a', {"data-field-name" : "name"}):
+        url = activityurls['href']
+        urllist.append(url)
+        activitycounter += 1
+    print('Found activities: ' + str(activitycounter))
+    try:
+        time.sleep(3)
+        next_page_button = driver.find_element_by_class_name('next_page')
+        next_page_button.click()
+    except:
+        print(urllist)
+        break
+
+
+print(str(len(urllist)) + ' Activities Found')
+
+correctedcounter = 0
+for urls in urllist:
+    driver.get(url)
+    time.sleep(1)
+    driver.find_element_by_id('distance-adjusted-help').click()
+
+    try:
+        driver.find_element_by_link_text('Correct Distance').click()
+        print('Corrected Distance on: ' + urls)
+        correctedcounter += 1
+    except:
+        print('Already Corrected: ' + urls)
+        continue
+print(str(correctedcounter) + ' / ' + str(len(urllist)) + 'Activities Corrected')
