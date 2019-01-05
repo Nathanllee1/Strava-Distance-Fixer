8a8516749bd5fb7827a561490f3843b57798f384
Nathan Lee <nathan.l.lee123@gmail.com>
2019-01-04 19:53:42 -0800

    Removed signin credentials

--- corrector.py
+++ corrector.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import time
 driver = webdriver.Firefox()
 
-email = 'nathan.l.lee123@gmail.com'#raw_input('Email: ')
-password = '3xFXwaK951od'#raw_input('Password: ')
+email = raw_input('Email: ')
+password = raw_input('Password: ')
 
 
 driver.get('https://www.strava.com/athlete/training')
@@ -13,38 +13,47 @@ driver.get('https://www.strava.com/athlete/training')
 driver.find_element_by_id('email').send_keys(email)
 driver.find_element_by_id('password').send_keys(password)
 driver.find_element_by_id('login-button').click()
-print('Signed in')
+
+if driver.current_url == 'https://www.strava.com/athlete/training':
+    print('Signed in')
+
 time.sleep(1.5)
 
-#parse with bs
 
 
 urllist = []
 activitycounter = 0
-
+pagecounter = 1
 while True:
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+    # get urls
     for activityurls in soup.find_all('a', {"data-field-name" : "name"}):
         url = activityurls['href']
+        print(url)
         urllist.append(url)
         activitycounter += 1
-    print('Found activities: ' + str(activitycounter))
+
+    print('Found activities: ' + str(activitycounter) + ' Page ' + 'pagecounter')
+
+    # click button
     try:
-        time.sleep(3)
         next_page_button = driver.find_element_by_class_name('next_page')
         next_page_button.click()
     except:
         print(urllist)
         break
+    pagecounter += 1
 
 
 print(str(len(urllist)) + ' Activities Found')
 
 correctedcounter = 0
 for urls in urllist:
-    driver.get(url)
+    while driver.current_url != urls:
+        driver.get(url)
+
     time.sleep(1)
     driver.find_element_by_id('distance-adjusted-help').click()
 
@@ -55,4 +64,4 @@ for urls in urllist:
     except:
         print('Already Corrected: ' + urls)
         continue
-print(str(correctedcounter) + ' / ' + str(len(urllist)) + 'Activities Corrected')
+print(str(correctedcounter) + ' / ' + str(len(urllist)) + ' Activities Corrected')
